8bbe0ea0bdc5a812038e874a763c717c81688fc0
Tyler Kuck <kuckti15@gmail.com>
2012-05-07 03:30:46 -0400

    Cleaning up.

--- Dashboard/build/main.d
+++ Dashboard/build/main.d
@@ -1,8 +1,11 @@
 main.o: /home/tyler/Desktop/XDash/Dashboard/source/main.c \
+ /home/tyler/Desktop/XDash/Dashboard/source/main.h \
  /usr/local/xenon/usr/include/elf/elf.h \
  /usr/local/xenon/usr/include/input/input.h \
  /usr/local/xenon/usr/include/xenon_soc/xenon_power.h \
  /usr/local/xenon/usr/include/xenon_smc/xenon_smc.h
+
+/home/tyler/Desktop/XDash/Dashboard/source/main.h:
 
 /usr/local/xenon/usr/include/elf/elf.h:
 
